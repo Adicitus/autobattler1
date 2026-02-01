c28120f3dc10c6adc359f1fe789e5bfb1f924b3c
Joakim Olsson <joakim.olsson@adicitus.cloud>
2026-02-01 08:46:26 +0100

    Added docstrings, added option to disable events
    - CampaignEvents now have a 'enabled' property that can be used to
      disable the event.

--- campaign.py
+++ campaign.py
@@ -10,13 +10,30 @@ class Door: pass
 class Campaign: pass
 
 class CampaignEvent:
+    """
+    Wrapper around basic callables when used in Campaign eventing.
+    """
     def __init__(self, callback:Callable[[CampaignAsset, Any], None]) -> None:
+        self.enabled: bool = True
+        """Determines if this event can start or not."""
         self.callback: Callable[[CampaignAsset, Any], None] = callback
+        """Callable to run then this event starts"""
 
-    def start(self, caller, event_data:any=None):
-        self.callback(caller, event_data)
+    def start(self, caller:CampaignAsset, event_data:any=None):
+        """
+        Calls the wrapped callable if this event is enabled, otherwise does nothing.
+
+        The callable is called using the given 'caller' as the first argument, and the given event_data as the second argument:
+            callable(caller, event_data)
+        """
+        if self.enabled:
+            self.callback(caller, event_data)
 
 class CampaignAsset:
+    """
+    Basic asset used in campaigns, implements .tick and basic event emitter functionality.
+    TODO: Inherit pymitter (https://pypi.org/project/pymitter/)?
+    """
     def __init__(self, name:str="") -> None:
         self.name: str = name
         self.events: dict[str, list[Callable[[CampaignAsset, Any], None]]] = {
@@ -24,6 +41,11 @@ class CampaignAsset:
         }
 
     def on(self, event_type: str, event: CampaignEvent | Callable[[CampaignAsset, Any], None]) -> None:
+        """
+        Adds the given callable or CampaignEvent to the list of event handlers for the given event_type.
+
+        Note: This method can be used to add event handlers for events not emitted by this object.
+        """
         e = event
         if not isinstance(e, CampaignEvent):
             if "__call__" in dir(e):
@@ -39,7 +61,12 @@ class CampaignAsset:
         
         self.events[event_type].append(e)
     
-    def off(self, event_type, event: CampaignEvent | Callable[[CampaignAsset, Any], None]):
+    def off(self, event_type:str, event: CampaignEvent | Callable[[CampaignAsset, Any], None]) -> None:
+        """
+        Removes the given callable or CampaignEvent as handlers for the given event_type.
+
+        Does nothing if the given event has not been added for the event_type.
+        """
         if event_type not in self.events:
             return
         
@@ -53,7 +80,12 @@ class CampaignAsset:
         self.events[event_type] = list(filter(lambda e: e.callback != event, self.events[event_type]))
 
     
-    def emit(self, event_type:str, event_data:Any=None):
+    def emit(self, event_type:str, event_data:Any=None) -> None:
+        """
+        Calls any CampaignEvent objects registered for the given event_type, using the given event_data.
+
+        Does nothing if no CampaignEvents are registered for the event_type. 
+        """
         if event_type not in self.events:
             return
         
@@ -61,6 +93,9 @@ class CampaignAsset:
             e.start(self, event_data)
     
     def tick(self):
+        """
+        Advances any game logic associated with this asset. Emits the "tick" event.
+        """
         self.emit("tick")
 
 class Walker(CampaignAsset):
@@ -85,7 +120,10 @@ class Walker(CampaignAsset):
         self.room: Room = starting_room
         self.room.enter(self)
 
-    def tick(self):
+    def tick(self) -> None:
+        """
+        Emits the "tick" event, then tries to enter the door given by self.door_select (unless it returns None).
+        """
         super().tick()
 
         self.ticks_passed += 1
@@ -114,10 +152,18 @@ class Room(CampaignAsset):
             self.on("enter", e)
         self.visited = False
     
-    def add_door(self, door):
+    def add_door(self, door:Door) -> None:
+        """
+        Manually adds a single door instance to the room. This can be used to add special doors (like random teleportation).
+        """
         self.doors.append(door)
     
     def connect_to(self, room:Room) -> Tuple[Door, Door]:
+        """
+        Connects this room to another room by creating a new door in each room.
+
+        Returns a tuple with the new doors: The first door is the one added to this room, the second is the door added to the other room.
+        """
         door1 = Door(name=f"door", room=room)
         door2 = Door(name=f"door", room=self)
         self.add_door(door1)
@@ -125,9 +171,21 @@ class Room(CampaignAsset):
         return (door1, door2)
     
     def disconnect_from(self, room:Room) -> None:
+        """
+        Removes the door connecting this door to the given room.
+
+        Does not remove the door in the given room, it needs to be removed manually.
+        """
         self.doors = list(filter(lambda d: d.room != room, self.doors))
 
     def enter(self, walker:Walker) -> Room:
+        """
+        Adds the given walker to this room, if it is not already present.
+
+        If the walker is added to the room, the "enter" event is emitted.
+
+        Returns this room.
+        """
         if walker not in self.walkers:
             self.visited = True
             self.walkers.append(walker)
@@ -135,6 +193,11 @@ class Room(CampaignAsset):
         return self
     
     def leave(self, walker:Walker) -> None:
+        """
+        If the given walker is present in this room, removes the walker and emits the "leave" event.
+
+        If the walker is not present in this room, this method does nothing.
+        """
         if walker not in self.walkers:
             return
         self.walkers.remove(walker)
@@ -148,6 +211,11 @@ class Door(CampaignAsset):
         self.events["enter"] = []
     
     def enter(self, walker:Walker) -> Optional[Room]:
+        """
+        Attempts to pass the walker through this door, emitting the "enter" event.
+
+        Returns a Room object representing the room on the other side of the door, or None if passage is rejected.
+        """
         self.emit("enter", walker)
         if self.room == None:
             return None
